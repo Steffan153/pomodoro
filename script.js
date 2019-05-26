9d641b550bbd77097a0d79371f38bc5c7bfb5aa5
Caleb Miller <caleb@mathmammoth.com>
2019-05-25 20:47:05 -0600

    commit

--- script.js
+++ script.js
@@ -1,153 +1,153 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let myInt;
-  let myInt2;
-  let inPlay = false;
-  const qs = (...args) => document.querySelector(...args);
+let myInt;
+let myInt2;
+let inPlay = false;
+const qs = (...args) => document.querySelector(...args);
 
-  function playSound() {
-    qs('audio').play();
+function playSound() {
+  qs('audio').play();
+}
+
+function toTime(m) {
+  let mins = Math.floor(m);
+  let secs = (m - mins) * 60;
+  if (mins < 10) mins = "0" + mins.toString();
+  if (secs < 10) secs = "0" + secs.toString();
+  return mins + ":" + secs;
+}
+
+function pause() {
+  inPlay = false;
+  playSound();
+}
+
+function resume() {
+  inPlay = true;
+  playSound();
+}
+
+function subTime(time) {
+  const arr = time.split(":");
+  let seconds = arr[1];
+  let mins = arr[0];
+  seconds--;
+  if (seconds < 0) {
+    seconds = 59;
+    mins--;
   }
+  if (seconds < 10) seconds = "0" + seconds;
+  let ntime = mins.toString() + ":" + seconds.toString();
+  if (ntime == "-1:59") ntime = "00:00";
+  return ntime;
+}
 
-  function toTime(m) {
-    let mins = Math.floor(m);
-    let secs = (m - mins) * 60;
-    if (mins < 10) mins = "0" + mins.toString();
-    if (secs < 10) secs = "0" + secs.toString();
-    return mins + ":" + secs;
-  }
+const bl = qs("#bl");
+const sl = qs("#sl");
+const p = qs("#progress");
 
-  function pause() {
-    inPlay = false;
-    playSound();
-  }
+function ce(event) {
+  if (event.keyCode == 13) go();
+}
 
-  function resume() {
-    inPlay = true;
-    playSound();
-  }
-
-  function subTime(time) {
-    const arr = time.split(":");
-    let seconds = arr[1];
-    let mins = arr[0];
-    seconds--;
-    if (seconds < 0) {
-      seconds = 59;
-      mins--;
+function go() {
+  clean();
+  playSound();
+  p.innerHTML =
+    `<p><i class="material-icons">work</i> Session:</p>
+    <div class="mdl-progress mdl-js-progress" id="prog"></div>
+    <p class="mt-3">
+      <span id="time">${toTime(sl.value)}</span> 
+      time left
+    </p>`;
+  componentHandler.upgradeElement(qs("#prog"));
+  let secs = 1;
+  myInt = setInterval(function() {
+    if (inPlay) {
+      qs("#time").innerHTML = subTime(qs("#time").innerHTML);
+      qs("#prog").MaterialProgress.setProgress((secs / (sl.value * 60)) * 100);
+      if (secs <= sl.value * 60) {
+        secs++;
+      } else {
+        secs = 1;
+        qs("#prog").MaterialProgress.setProgress(0);
+        clearInterval(myInt);
+        doBreak();
+      }
     }
-    if (seconds < 10) seconds = "0" + seconds;
-    let ntime = mins.toString() + ":" + seconds.toString();
-    if (ntime == "-1:59") ntime = "00:00";
-    return ntime;
-  }
+  }, 1000);
+}
 
-  const bl = qs("#bl");
-  const sl = qs("#sl");
-  const p = qs("#progress");
+function clean() {
+  clearInterval(myInt);
+  clearInterval(myInt2);
+  qs("#sl").disabled = true;
+  qs("#bl").disabled = true;
+  resume();
+}
 
-  function ce(event) {
-    if (event.keyCode == 13) go();
-  }
-
-  function go() {
-    clean();
-    playSound();
-    p.innerHTML =
-      `<p><i class="material-icons">work</i> Session:</p>
-      <div class="mdl-progress mdl-js-progress" id="prog"></div>
-      <p class="mt-3">
-        <span id="time">${toTime(sl.value)}</span> 
-        time left
-      </p>`;
-    componentHandler.upgradeElement(qs("#prog"));
-    let secs = 1;
-    myInt = setInterval(function() {
-      if (inPlay) {
-        qs("#time").innerHTML = subTime(qs("#time").innerHTML);
-        qs("#prog").MaterialProgress.setProgress((secs / (sl.value * 60)) * 100);
-        if (secs <= sl.value * 60) {
-          secs++;
-        } else {
-          secs = 1;
-          qs("#prog").MaterialProgress.setProgress(0);
-          clearInterval(myInt);
-          doBreak();
-        }
+function doBreak() {
+  clean();
+  playSound();
+  p.innerHTML =
+    `<p><i class="material-icons">free_breakfast</i> Break!</p>
+    <div class="mdl-progress mdl-js-progress" id="prog"></div>
+    <p class="mt-3">
+      <span id="time">${toTime(bl.value)}</span> 
+      time left
+    </p>`;
+  componentHandler.upgradeElement(qs("#prog"));
+  let secs = 1;
+  myInt2 = setInterval(function() {
+    if (inPlay) {
+      qs("#time").innerHTML = subTime(qs("#time").innerHTML);
+      qs("#prog").MaterialProgress.setProgress((secs / (bl.value * 60)) * 100);
+      if (secs <= bl.value * 60) {
+        secs++;
+      } else {
+        secs = 1;
+        qs("#prog").MaterialProgress.setProgress(0);
+        clearInterval(myInt2);
+        go();
       }
-    }, 1000);
-  }
+    }
+  }, 1000);
+};
 
-  function clean() {
-    clearInterval(myInt);
-    clearInterval(myInt2);
-    qs("#sl").disabled = true;
-    qs("#bl").disabled = true;
-    resume();
-  }
+function stop() {
+  playSound();
+  clearInterval(myInt);
+  clearInterval(myInt2);
+  qs("#sl").disabled = false;
+  qs("#bl").disabled = false;
+  qs('#progress').innerHTML = '';
+  inPlay = false;
+}
 
-  function doBreak() {
-    clean();
-    playSound();
-    p.innerHTML =
-      `<p><i class="material-icons">free_breakfast</i> Break!</p>
-      <div class="mdl-progress mdl-js-progress" id="prog"></div>
-      <p class="mt-3">
-        <span id="time">${toTime(bl.value)}</span> 
-        time left
-      </p>`;
-    componentHandler.upgradeElement(qs("#prog"));
-    let secs = 1;
-    myInt2 = setInterval(function() {
-      if (inPlay) {
-        qs("#time").innerHTML = subTime(qs("#time").innerHTML);
-        qs("#prog").MaterialProgress.setProgress((secs / (bl.value * 60)) * 100);
-        if (secs <= bl.value * 60) {
-          secs++;
-        } else {
-          secs = 1;
-          qs("#prog").MaterialProgress.setProgress(0);
-          clearInterval(myInt2);
-          go();
-        }
-      }
-    }, 1000);
-  };
+let settingsBtn = qs('.settings-button');
+let dialog = qs('dialog');
 
-  function stop() {
-    playSound();
-    clearInterval(myInt);
-    clearInterval(myInt2);
-    qs("#sl").disabled = false;
-    qs("#bl").disabled = false;
-    qs('#progress').innerHTML = '';
-    inPlay = false;
-  }
+if (!dialog.showModal) {
+  dialogPolyfill.registerDialog(dialog);
+}
 
-  let settingsBtn = qs('.settings-button');
-  let dialog = qs('dialog');
+settingsBtn.addEventListener('click', function() {
+  dialog.showModal();
+});
 
-  if (!dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
-  }
+if (localStorage.getItem('session-length')) {
+  qs('#sl').value = localStorage.getItem('session-length');
+}
 
-  settingsBtn.addEventListener('click', function() {
-    dialog.showModal();
-  });
+if (localStorage.getItem('break-length')) {
+  qs('#bl').value = localStorage.getItem('break-length');
+}
 
-  if (localStorage.getItem('session-length')) {
-    qs('#sl').value = localStorage.getItem('session-length');
-  }
+dialog.querySelector('button').addEventListener('click', function() {
+  localStorage.setItem('session-length', qs('#sl').value);
+  localStorage.setItem('break-length', qs('#bl').value);
+  dialog.close();
+});
 
-  if (localStorage.getItem('break-length')) {
-    qs('#bl').value = localStorage.getItem('break-length');
-  }
-
-  dialog.querySelector('button').addEventListener('click', function() {
-    localStorage.setItem('session-length', qs('#sl').value);
-    localStorage.setItem('break-length', qs('#bl').value);
-    dialog.close();
-  });
-
+document.addEventListener('DOMContentLoaded', function() {
   let searchParams = new URLSearchParams(location.search);
 
   if (searchParams.get('pomo')) {
